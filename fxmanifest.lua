d0bdc8e324c5e484fa1d3133808974577c84d02a
ZokuRush <67478346+ZokuRush@users.noreply.github.com>
2022-07-29 23:51:26 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
 
-
-description 'hypr9nitro'
-
 server_scripts {
-	'server/main.lua'
+	'server/sv_main.lua'
 }
 
 client_scripts {
-	'client/main.lua'
+	'client/cl_main.lua'
 }
